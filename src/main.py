b0e8e62501ad68e6ddc48ef7d54e7b14b7674d9a
Desmon18 <antaodesmon@gmail.com>
2026-03-16 08:01:04 +0000

    Print bus arrival times using stop ID

--- src/main.py
+++ src/main.py
@@ -1,15 +1,31 @@
 import requests
 
-# TFL API endpoint
-url = "https://api.tfl.gov.uk/Line/Mode/tube/Status"
+lat = 51.48074
+lon = -0.43309
 
-# send request to TFL API
+# Bus Stop ID 
+stop_id = "490007811B"
+
+# Get nearby bus stops using the TFL API
+# url = f"https://api.tfl.gov.uk/StopPoint?lat={lat}&lon={lon}&stopTypes=NaptanPublicBusCoachTram&radius=300"
+
+# TFL API to get bus stop information by stop ID
+url = f"https://api.tfl.gov.uk/StopPoint/{stop_id}/Arrivals"
+
 response = requests.get(url)
+data = response.json()
 
-# check if the request was successful
-if response.status_code == 200:
-    data = response.json()
-    for line in data:
-        print(f"Line: {line['name']}")
-        print(f"Status: {line['lineStatuses'][0]['statusSeverityDescription']}")
+# sort bus arrivals by expected arrival time
+data = sorted(data, key=lambda x: x["expectedArrival"])
 
+print(f"Bus arrivals at stop {stop_id}:\n")
+
+for bus in data[:5]:
+    line = bus["lineName"]
+    destination = bus["destinationName"]
+    time = bus["timeToStation"] // 60  # convert seconds to minutes
+
+    print(f"Line {line} to {destination} arriving in {time} minutes")
+
+# for stop in data["stopPoints"]:
+#     print(stop["commonName"], stop["id"])
